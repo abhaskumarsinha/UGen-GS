9c4bb1c029e024f22450ac6ceaa3dfcbdd62954f
Abhas Kumar Sinha <31654395+abhaskumarsinha@users.noreply.github.com>
2026-02-16 20:56:11 +0530

    Import additional algorithms in __init__.py

--- UGen/encoder/algorithms/__init__.py
+++ UGen/encoder/algorithms/__init__.py
@@ -1,1 +1,4 @@
-
+from UGen.encoder.algorithms.greedy import *
+from UGen.encoder.algorithms.quadtree import *
+from UGen.encoder.algorithms.quadtree_densify import *
+from UGen.encoder.algorithms.wmp import *
