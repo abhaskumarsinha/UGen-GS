251ecf7bef17bfac45bf08588158c203f343c57d
Abhas Kumar Sinha <31654395+abhaskumarsinha@users.noreply.github.com>
2026-02-22 15:30:42 +0530

    Update __init__.py

--- UGen/decoder/__init__.py
+++ UGen/decoder/__init__.py
@@ -1,4 +1,7 @@
 from UGen.decoder.base import *
-from UGen.decoder.gaussian_rasterizer import *
 from UGen.decoder.numpy_rasterizer import *
 from UGen.decoder.numpy_gaussian_rasterizer import *
+try:
+  from UGen.decoder.gaussian_rasterizer import *
+except:
+  pass
